00aeeb77b7642e28d382f76d4b28daf7a64313f3
ilyeshr2 <ilyesharrache0@gmail.com>
2024-08-22 16:45:21 +0100

    update the way to call the pinecone api since they changed it

--- app/services/pinecone_service.py
+++ app/services/pinecone_service.py
@@ -1,4 +1,4 @@
-import pinecone
+from pinecone import Pinecone, ServerlessSpec
 from app.services.huggingface_services import get_embedding
 from dotenv import load_dotenv
 import os
@@ -7,8 +7,8 @@ import os
 load_dotenv()
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 
-# make sure to enter your actual Pinecone environment
-pinecone.init(api_key=PINECONE_API_KEY, environment='gcp-starter') 
+# Create an instance of the Pinecone class
+pc = Pinecone(api_key=PINECONE_API_KEY) 
 
 EMBEDDING_DIMENSION = 384
 
@@ -16,21 +16,34 @@ def embed_chunks_and_upload_to_pinecone(chunks, index_name):
     
     # delete the index if it already exists. 
     # as Pinecone's free plan only allows one index
-    if index_name in pinecone.list_indexes():
-        pinecone.delete_index(name=index_name)
+    if index_name in pc.list_indexes().names():
+        pc.delete_index(name=index_name)
     
     # create a new index in Pinecone
     # the EMBEDDING_DIMENSION is based on what 
     # your embedding model outputs for all-MiniLM-L6-v2 it is 384
-    pinecone.create_index(name=index_name,
-                          dimension=EMBEDDING_DIMENSION, metric='cosine')
-    index = pinecone.Index(index_name)
-    # embed each chunk and aggregate these embeddings
+    pc.create_index(
+        name=index_name,
+        dimension=EMBEDDING_DIMENSION,
+        metric='cosine',
+        spec=ServerlessSpec(
+            cloud='aws', 
+            region='us-east-1'
+            )  # Adjust according to your needs
+    )
+    
+    # Access the created index
+    index = pc.Index(index_name)
+    
+    # Embed each chunk and aggregate these embeddings
     embeddings_with_ids = []
     for i, chunk in enumerate(chunks):
         embedding = get_embedding(chunk)
         embeddings_with_ids.append((str(i), embedding, chunk))
-    # upload the embeddings and relevant texts for each chunk
-    # to the Pinecone index
+          
+        
+    # Prepare the data for upsertion
     upserts = [(id, vec, {"chunk_text": text}) for id, vec, text in embeddings_with_ids]
+    
+    # Upsert embeddings into Pinecone index
     index.upsert(vectors=upserts)
