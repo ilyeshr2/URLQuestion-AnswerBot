0e4db164aff2125568620cec3412c264f5c443ae
ilyeshr2 <ilyesharrache0@gmail.com>
2024-08-27 01:48:20 +0100

    Update openai_service.py

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 OPENAI_EMBEDDING_MODEL = 'text-embedding-ada-002'
+CHATGPT_MODEL = 'gpt-4-1106-preview'
 
 def get_embedding(chunk):
   url = 'https://api.openai.com/v1/embeddings'
@@ -20,3 +21,26 @@ def get_embedding(chunk):
   response_json = response.json()
   embedding = response_json["data"][0]["embedding"]
   return embedding
+
+def get_llm_answer(prompt):
+  # Aggregate a messages array to send to the LLM
+  messages = [{"role": "system", "content": "You are a helpful assistant."}]
+  messages.append({"role": "user", "content": prompt})
+  # Send the payload to the LLM to retrieve an answer
+  url = 'https://api.openai.com/v1/chat/completions'
+  headers = {
+      'content-type': 'application/json; charset=utf-8',
+      'Authorization': f"Bearer {OPENAI_API_KEY}"            
+  }
+  data = {
+      'model': CHATGPT_MODEL,
+      'messages': messages,
+      'temperature': 1, 
+      'max_tokens': 1000
+  }
+  response = requests.post(url, headers=headers, data=json.dumps(data))
+  
+  # return the final answer
+  response_json = response.json()
+  completion = response_json["choices"][0]["message"]["content"]
+  return completion
